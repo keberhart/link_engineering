08242e91fd89d5b5e931c05858a08726ef56e298
Kyle Eberhart <kyle.eberhart@gmail.com>
2023-05-29 00:49:59 -0800

    Adding support for devices

--- link_eng.py
+++ link_eng.py
@@ -521,3 +521,37 @@ def statgain(G_max, phi):
             return 53-(G_max/2)-25*math.log(phi)
         if (phi_b3 < phi <= 180):
             return 0
+
+class Device():
+    ''' Describe a device in an antenna system
+
+        Requires: a name string
+        Accepts: floating point values for gain and noise_figure or temperature
+
+        gain defaults to 0.0
+    '''
+
+    def __init__(self, name, gain=0.0, temperature=260.0, noise_figure=None):
+        self.name = name
+        self.gain = gain
+        if (temperature == None) and (noise_figure == None):
+            error_msg = 'A Noise Figure or Temperature must be supplied.'
+            raise Exception(error_msg)
+        if noise_figure != None:
+            self.noise_figure = noise_figure
+            self.temperature = NF_to_T_noise(self.noise_figure)
+        else:
+            self.temperature = temperature
+            self.noise_figure = T_noise_to_NF(self.temperature)
+
+    def __repr__(self):
+        return "Device({}, {}, {})".format(self.name, self.temperature,
+                                           self.noise_figure)
+    
+    def __str__(self):
+        line1 = '\nDevice:\t{}'.format(self.name)
+        line2 = '\n\tGain:\t{}dB'.format(self.gain)
+        line3 = '\n\tT:\t{}K'.format(self.temperature)
+        line4 = '\n\tNF:\t{}dB'.format(self.noise_figure)
+        return (line1 + line2 + line3 + line4)
+
